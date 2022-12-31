340706da90737593cde8bb4926a8b707ab277bdc
othernew1995 <othernew1995@gmail.com>
2022-12-31 12:32:00 +0530

    home page
    i have completed my home page work please review it
    
    thanks
    team lead testing

---
+++ testproject/src/homepackage/HomePageClass.java
@@ -0,0 +1,8 @@
+package homepackage;
+
+public class HomePageClass {
+	public static void main(String [] args) {
+		System.out.println("new project created");
+	}
+
+}
---
+++ testproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author NIRANJAN
+ *
+ */
+module testproject {
+}
